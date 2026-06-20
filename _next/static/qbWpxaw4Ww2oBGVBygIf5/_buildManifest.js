460e68a428b9c9ed8e6641796d625776afa3f5fa
Aman Kumar <aman24052001@gmail.com>
2026-06-20 10:45:14 +0000

    Deploy: static export of Vastra ERP frontend

---
+++ _next/static/qbWpxaw4Ww2oBGVBygIf5/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/vastra-erp//_next/:path+",
+        "destination": "/vastra-erp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
